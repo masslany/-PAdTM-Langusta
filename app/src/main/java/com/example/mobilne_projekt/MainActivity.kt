4d1515b2cdac387f34cd3f98bbb95045dce8e134
masslany <p.atryk@outlook.com>
2020-04-15 12:37:02 +0200

    init

---
+++ app/src/main/java/com/example/mobilne_projekt/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mobilne_projekt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
