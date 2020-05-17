478a0608db7a0b42c8115a74726638f8a4a039fe
Daniel Wacewicz <daniorocket@gmail.com>
2020-05-17 13:41:57 +0200

    Added button to navigate to courseAddFragment on HomeFragment

--- app/src/main/java/com/example/mobilne_projekt/ui/CourseEditFragment.kt
+++ app/src/main/java/com/example/mobilne_projekt/ui/CourseEditFragment.kt
@@ -1,11 +1,11 @@
 package com.example.mobilne_projekt.ui
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 
 import com.example.mobilne_projekt.R
 
@@ -26,7 +26,7 @@ class CourseEditFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(CourseEditViewModel::class.java)
+        viewModel = ViewModelProvider(this).get(CourseEditViewModel::class.java)
         // TODO: Use the ViewModel
     }
 
